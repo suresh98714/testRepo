811f55c9a13de11f21df200f9a4bdcfb34c4f58a
suresh <btech.sharma@gmail.com>
2019-06-30 00:26:27 +0530

    commit

--- src/main/java/pages/GoogleSearchPage.java
+++ src/main/java/pages/GoogleSearchPage.java
@@ -19,7 +19,8 @@ public class GoogleSearchPage {
 	WebElement googleSearch;
 	
 	public void googleeSearch(){
-		googleSearch.sendKeys("Google Testing",Keys.ENTER);		
+		googleSearch.sendKeys("Google Testing",Keys.ENTER);	
+		System.out.println("Google search text is Entered");
 	}
 
 }
