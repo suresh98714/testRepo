fe2c0cb3959ff367c00c92414f29447390db8ca0
suresh <btech.sharma@gmail.com>
2019-07-05 00:49:28 +0530

    add

--- src/main/java/pages/GoogleSearchPage.java
+++ src/main/java/pages/GoogleSearchPage.java
@@ -21,6 +21,7 @@ public class GoogleSearchPage {
 	public void googleeSearch(){
 		googleSearch.sendKeys("Google Testing",Keys.ENTER);
 		System.out.println("Entered text in Google Search");
+		System.out.println("Google Test Done");
 	}
 
 }
