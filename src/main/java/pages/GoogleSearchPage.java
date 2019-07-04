47c936213780c02514870a7b8ca0a862e7f94273
suresh <btech.sharma@gmail.com>
2019-07-05 00:10:12 +0530

    2nd commit add Remote

--- src/main/java/pages/GoogleSearchPage.java
+++ src/main/java/pages/GoogleSearchPage.java
@@ -22,6 +22,7 @@ public class GoogleSearchPage {
 		googleSearch.sendKeys("Google Testing",Keys.ENTER);
 		System.out.println("Entered text in Google Search");
 		System.out.println("Google search Done");
+		System.out.println("Google Test Finishes");
 	}
 
 }
